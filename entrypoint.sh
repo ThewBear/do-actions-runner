bf5d6364fb5ab64950a59c3bd55bd2a227020bdc
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-01-05 22:56:51 +0700

    Add ORG support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,33 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-RUNNER_TOKEN=$(curl -s -X POST -H "authorization: token ${TOKEN}" "https://api.github.com/repos/${OWNER}/${REPO}/actions/runners/registration-token" | jq -r .token)
+if [ -z "${TOKEN:-}" ]
+then
+  echo "TOKEN is required"
+  exit 1
+fi
+
+if [ -n "${ORG:-}" ]
+then
+  API_PATH=orgs/${ORG}
+  CONFIG_PATH=${ORG}
+elif [ -n "${OWNER:-}" ] && [ -n "${REPO:-}" ]
+then
+  API_PATH=repos/${OWNER}/${REPO}
+  CONFIG_PATH=${OWNER}/${REPO}
+else
+  echo "[ORG] or [OWNER and REPO] is required"
+  exit 1
+fi
+
+RUNNER_TOKEN=$(curl -s -X POST -H "authorization: token ${TOKEN}" "https://api.github.com/${API_PATH}/actions/runners/registration-token" | jq -r .token)
 
 cleanup() {
   ./config.sh remove --token "${RUNNER_TOKEN}"
 }
 
 ./config.sh \
-  --url "https://github.com/${OWNER}/${REPO}" \
+  --url "https://github.com/${CONFIG_PATH}" \
   --token "${RUNNER_TOKEN}" \
   --name "${NAME:-$(hostname)}" \
   --unattended
